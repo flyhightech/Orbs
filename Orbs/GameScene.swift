96ff30208e23a12169e0fba7635ee40edd4590f5
Bernard Huff <bernard.huff008@gmail.com>
2018-08-09 20:38:38 -0500

    Added the player to the scene

--- Orbs/GameScene.swift
+++ Orbs/GameScene.swift
@@ -12,6 +12,9 @@ import GameplayKit
 class GameScene: SKScene {
     
     var tracksArray:[SKSpriteNode]? = [SKSpriteNode]()
+    var player:SKSpriteNode?
+   
+//    Below is the array that sets up the tracks in the scene.
     
     func setupTracks() {
         for i in 0...8 {
@@ -21,10 +24,17 @@ class GameScene: SKScene {
         }
     }
     
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        guard let playerPosition = tracksArray?.first?.position.x else { return }
+        player?.position = CGPoint(x: playerPosition, y: self.size.height / 2)
+        
+        self.addChild(player!)
+    }
+    
     override func didMove(to view: SKView) {
         setupTracks()
-        
-        tracksArray?.last?.color = UIColor.yellow
+        createPlayer()
         
     }
     
